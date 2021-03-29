542470e4ad5bd07beead289bb53d11594842ef7b
Kozer2 <benhemann@yahoo.com>
2021-03-29 18:05:57 -0500

    finished tests

--- Lab06-UnitTest/UnitTest1.cs
+++ Lab06-UnitTest/UnitTest1.cs
@@ -46,5 +46,87 @@ namespace Lab06_UnitTest
             //assert
             Assert.Equal("Looking Pretty Fly", result);
         }
+
+
+        [Fact]
+        public void Human_can_override_eat()
+        {
+            // arrange
+            Human human = new Human();
+
+            // act
+            string result = human.Eat();
+
+            //assert
+            Assert.Equal("Lets go drink water so we are not hungry and save money to buy bitcoin.", result);
+        }
+
+        [Fact]
+        public void Naked_mole_can_sleep()
+        {
+            // arrange
+            NakedMoleRat nakedmoleRat = new NakedMoleRat();
+
+            // act
+            bool result = nakedmoleRat.SleepIsTrue;
+
+            //assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Naked_mole_rat_is_naked()
+        {
+            // arrange
+            NakedMoleRat nakedmoleRat = new NakedMoleRat();
+
+            // act
+            string result = nakedmoleRat.SkinType;
+
+            //assert
+            Assert.Equal("Naked", result);
+        }
+
+        [Fact]
+        public void Sea_turtle_has_shell()
+        {
+            // arrange
+            SeaTurtle seaTurtle = new SeaTurtle();
+
+            // act
+            string result = seaTurtle.SkinType;
+
+            //assert
+            Assert.Equal("Shell", result);
+        }
+
+
+        [Fact]
+        public void Sea_turtle_lays_eggs()
+        {
+            // arrange
+            SeaTurtle seaTurtle = new SeaTurtle();
+
+            // act
+            bool result = seaTurtle.LaysEggs;
+
+            //assert
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void Viper_gives_live_birth_lays_eggs_false()
+        {
+            // arrange
+            Viper viper = new Viper();
+
+            // act
+            string result = viper.IsColdBlooded();
+
+            //assert
+            Assert.Equal("Where's the sun? I am cold!", result);
+        }
+
     }
 }
